3feb9aa0e7d80c24ec80db28c13989ca9953028d
youshu0921 <hucheng0828@gmail.com>
2026-04-21 09:29:38 +0800

    Delete youshu0417 directory

--- youshu0417/cloudfunctions/getUserInfo/index.js
+++
@@ -1,5 +0,0 @@
-const cloud = require("wx-server-sdk")
-cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV })
-exports.main = async (event, context) => {
-  return { openid: cloud.getWXContext().OPENID }
-}
